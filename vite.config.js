4dcd33a1e3a33cadcd65389bacce6ac559f11600
Waqar Ahmad <wiki.mir@gmail.com>
2024-05-09 15:27:58 +0500

    dfsf

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ["@fortawesome/react-fontawesome"],
+  build: {
+    rollupOptions: {
+      external: ["@fortawesome/react-fontawesome"],
+    },
   },
 })
