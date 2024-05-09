110aa021d849b44a882605da3a3c646e8238d236
Waqar Ahmad <wiki.mir@gmail.com>
2024-05-09 16:24:10 +0500

    fds

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: [
-        "@fortawesome/react-fontawesome",
-        "@fortawesome/free-solid-svg-icons",
-      ],
+      external: ["@fortawesome/free-solid-svgds-icons"],
     },
   },
 })
