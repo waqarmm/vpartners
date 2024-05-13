7fbf0ef6b145d6d580ca51d8ddde7a22353da103
Waqar Ahmad <wiki.mir@gmail.com>
2024-05-13 19:23:49 +0500

    new

--- vite.config.js
+++ vite.config.js
@@ -6,18 +6,6 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["@fortawesome/free-solid-svgds-icons"],
-    },
-  },
-})
-import {defineConfig} from "vite"
-import react from "@vitejs/plugin-react"
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
       ////////////
       external: ["@fortawesome/free-solid-svgds-icons"],
     },
